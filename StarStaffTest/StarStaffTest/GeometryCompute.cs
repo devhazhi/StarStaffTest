d1caa44f7ae27d4340ecda0d9020c0d91779f7bd
devhazhi <dmhazhi@gmail.com>
2017-07-18 10:01:42 +0300

    Test Task

---
+++ StarStaffTest/StarStaffTest/GeometryCompute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarStaffTest
+{
+    public static class GeometryCompute
+    {
+      public static double getTriangleArea(uint nSizeA_, uint nSizeB_, uint nSizeC_) {
+         bool bCanComputing = false;
+         if (nSizeA_ > nSizeB_ && nSizeA_ > nSizeC_)
+            bCanComputing = nSizeA_ <= nSizeB_ + nSizeC_;
+         else if (nSizeB_ > nSizeC_)
+            bCanComputing = nSizeB_ <= nSizeA_ + nSizeC_;
+         else
+            bCanComputing = nSizeC_ <= nSizeA_ + nSizeB_;
+         if (!bCanComputing)
+            throw new ArgumentException("Стороны треугольника заданы неверно!");
+
+         var ffPerimetr2 = (nSizeA_ + nSizeB_ + nSizeC_) / 2.0;
+         return 
+            Math.Sqrt(
+               ffPerimetr2 
+                  * (ffPerimetr2 - nSizeA_) 
+                  * (ffPerimetr2 - nSizeB_) 
+                  * (ffPerimetr2 - nSizeC_));
+      }
+    }
+}
---
+++ sqltask.sql
@@ -0,0 +1,12 @@
+﻿SELECT DISTINCT cb.Name 
+FROM 
+ProductBuys pb  
+INNER JOIN Customers c 
+ON c.CustomerId == pb.CustomerId AND pb.ProductName="молоко" 
+WHERE
+c.CustomerId = 
+	(SELECT TOP 1 pbuy.CustomerId 
+		FROM ProductBuys pbuy 
+		WHERE pbuy.ProductName = "сметана" AND pbuy.PurchaiseDatetime > DATEADD(month, -1, getutcdate())
+	)
+	
